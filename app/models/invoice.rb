80bf8b846d0865cb480d3d6dcd6f635b9a042c66
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-02 17:21:45 -0400

    Fix invoice_num compute callback for first entry

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,7 @@ class Invoice < ActiveRecord::Base
       belongs_to :client
 
       def compute_invoice_num
+          self.invoice_number = ( (1).to_s + Time.now.year.to_s ).to_i unless Invoice.exists?(1)
           self.invoice_number = ( (Invoice.last.id + 1).to_s + Time.now.year.to_s ).to_i
       end
 
