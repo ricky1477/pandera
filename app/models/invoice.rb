2a725fc923bd30b403a7e68b42b0c6baaacfa1f3
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-08 13:51:58 -0400

    Changing compute_invoice_num callback to before_save rather than before_create

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,14 +1,10 @@
 class Invoice < ActiveRecord::Base
-      before_create :compute_invoice_num
+      before_save :compute_invoice_num
       belongs_to :client
       validates :price, presence: true
 
       def compute_invoice_num
-          unless Invoice.all.length > 1
-            self.invoice_number = ( (1).to_s + Time.now.year.to_s ).to_i
-          else
-            self.invoice_number = ( (Invoice.last.id + 1).to_s + Time.now.year.to_s ).to_i
-          end
+          self.invoice_number = (Invoice.all.length <= 1) ? ((1).to_s + Time.now.year.to_s ).to_i : ( (Invoice.last.id + 1).to_s + Time.now.year.to_s ).to_i
       end
 
       def self.search(search)
