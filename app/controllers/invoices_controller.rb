3ce1ea66c209524168e4041fe3b3685a54e9f301
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-13 00:34:33 -0400

    Fix invoices views

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -42,9 +42,6 @@ class InvoicesController < ApplicationController
   # PATCH/PUT /invoices/1.json
   def update
     respond_to do |format|
-      Rails.logger.debug('-------')
-      Rails.logger.debug(params)
-      Rails.logger.debug(params[:invoice][:paid])
       if params[:invoice][:paid] == '1'
         @invoice.services.each do |service|
           service.paid = true
