3ce9e3f8ec21d55818a202cff015d2520488f72c
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-15 12:57:55 -0400

    Add search to services#index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,9 +4,8 @@ class ServicesController < ApplicationController
   # GET /services
   # GET /services.json
   def index
-    @services = Service.all
-    #@services = Service.search(params[:search]).order(sort_column + ' ' +
-    #  sort_direction).paginate(:per_page => 15, :page => params[:page])
+    @services = Service.search(params[:search]).order(sort_column + ' ' +
+      sort_direction).paginate(:per_page => 15, :page => params[:page])
   end
 
   # GET /services/1
@@ -27,7 +26,6 @@ class ServicesController < ApplicationController
   # POST /services.json
   def create
     @service = Service.new(service_params)
-
     respond_to do |format|
       if @service.save
         format.html { redirect_to @service, notice: 'Service was successfully created.' }
@@ -74,4 +72,12 @@ class ServicesController < ApplicationController
     def service_params
       params.require(:service).permit(:date, :description, :maturity, :client_id, :paid, :invoice_id, :price)
     end
+    def sort_column
+      Service.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
+
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,17 @@ class Service < ActiveRecord::Base
       belongs_to :client
       belongs_to :invoice
       validates :price, presence: true
+
+      def self.search(search)
+        if search
+			joins('JOIN clients ON clients.id = services.client_id').
+            where(' clients.name ILIKE ? OR CAST( date AS text ) ILIKE ?
+				OR CAST( price AS text ) ILIKE ? OR description ILIKE ?
+				OR CAST(maturity AS text) ILIKE ?',
+             "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+        else
+            where(nil)
+        end
+    end
+
 end
