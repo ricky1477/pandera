7cbc0813004254b54f9f6b917a2dd4da39df8311
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-15 12:01:43 -0400

    Fix invoice creating process

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,15 +1,25 @@
 class Invoice < ActiveRecord::Base
       before_save :compute_invoice_num
       before_save :link_services_to_invoice
+      before_save :compute_total
       belongs_to :client
       has_many :services
 
       def compute_invoice_num
-          self.invoice_number = (Invoice.all.length <= 1) ? ((1).to_s + Time.now.year.to_s ).to_i : ( (Invoice.last.id + 1).to_s + Time.now.year.to_s ).to_i
+          self.invoice_number = (Invoice.all.length <= 0) ? ((1).to_s + Time.now.year.to_s ).to_i : ( (Invoice.last.id + 1).to_s + Time.now.year.to_s ).to_i
       end
 
       def link_services_to_invoice
-        self.services = self.client.services.where("paid IS NOT TRUE")
+        self.services = self.client.services.where("paid IS NOT TRUE").where("invoice_id IS NULL")
+      end
+
+      def compute_total
+          prices = self.client.services.where("paid IS NOT TRUE").where("invoice_id IS NULL").pluck(:price)
+          if prices.length<=0
+            self.total = 0
+          else
+            self.total = prices.inject(0){|sum,x| sum + x }
+          end
       end
 
       def self.search(search)
