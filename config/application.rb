7538464a34b0457d2ce8fd9c0c7c1b2c1a8f85b8
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-20 14:37:19 -0400

    Adding more translations

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Pandera
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    I18n.available_locales = [:pt, :en]
   end
 end
