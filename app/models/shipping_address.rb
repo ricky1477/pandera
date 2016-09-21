17bfcda4d9e6e12a3b7b017e36d51a28db2da296
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-09-21 17:59:20 -0400

    Variable Shipping addresses - when save default = true, save all others as false

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,12 @@
 class ShippingAddress < ActiveRecord::Base
+    before_update :clear_defaults
+
+    def clear_defaults
+      if self.is_default_changed?
+        ShippingAddress.where.not(id: self.id).each do |sa|
+            sa.is_default = false
+            sa.save!
+        end
+      end
+    end
 end
