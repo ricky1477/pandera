13e3ffc0687f1b9fb8761bcde04f68cc83437fe7
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-10-10 17:07:12 -0400

    Tyring to add JS engine to capybara

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -18,16 +18,16 @@ feature 'home page' do
         find('#dynamic_select').find(:xpath, '//*[@id="dynamic_select"]/option[2]').select_option
         find('#dynamic_select').find(:xpath, '//*[@id="dynamic_select"]/option[2]').click
         select('Inglês', from: 'dynamic_select')
+        a = page.find('#dynamic_select').find(:xpath, '//*[@id="dynamic_select"]/option[2]').click
         pending # Still need to learn how this works, select is not reloading the page
         expect(page).to have_content('Home')
-        expect(page).to have_content('Clientes')
-        expect(page).to have_content('Serviços')
-        expect(page).to have_content('Faturas')
+        expect(page).to have_content('Clients')
+        expect(page).to have_content('Services')
+        expect(page).to have_content('Invoices')
         expect(page).to have_content('Login')
-        expect(page).to have_content('Bem vindo ao sistema de faturamento da Nice and Green Lanscaping!')
-        expect(page).to have_content('Escolha o idioma')
-        expect(page).to have_content('Inglês ')
-        expect(page).to have_content('Português')
+        expect(page).to have_content('Welcome to the Nice and Green Lanscaping billing system!')
+        expect(page).to have_content('Pick language')
+        expect(page).to have_content('English')
         expect(page).to have_content('Portuguese')
     end
 
