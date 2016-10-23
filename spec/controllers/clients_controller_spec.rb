3e91b186dac01b73279bf9c2849ed3e238fb6fdf
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-10-23 03:18:38 -0400

    Adding client#{update/destroy} controller tests

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -25,6 +25,48 @@ describe ClientsController do
 		end
 	end
 
+	describe "PUT update" do
+		let(:client)  {FactoryGirl.create(:client)}
+
+		context "valid data" do
+			let (:valid_data) { FactoryGirl.attributes_for(:client, name: 'newName') }
+
+			it "redirects to clients#show" do
+				put :update, id: client, client: valid_data
+				expect(response).to redirect_to(client)
+			end
+
+			it "updates client in database" do
+				put :update, id: client, client: valid_data
+				client.reload
+				expect(client.name).to eq('newName')
+			end
+		end
+		context "invalid data" do
+			let (:invalid_data) { FactoryGirl.attributes_for(:client, name: '') }
+			let (:invalid_data2) { FactoryGirl.attributes_for(:client, street_address: '') }
+
+			it "renders :edit template" do
+				put :update, id: client, client: invalid_data
+				expect(response).to render_template(:edit)
+			end
+			it "renders :edit template#{2}" do
+				put :update, id: client, client: invalid_data2
+				expect(response).to render_template(:edit)
+			end
+			it "does not creates new client in database" do
+				put :update, id: client, client: invalid_data
+				client.reload
+				expect(client.name).not_to eq('testName')
+			end
+			it "does not creates new client in database#{2}" do
+				put :update, id: client, client: invalid_data2
+				client.reload
+				expect(client.street_address).not_to eq('testStreet')
+			end
+		end
+	end
+
 	describe "GET new" do
 		it "renders :new template" do
 			get :new
@@ -40,7 +82,7 @@ describe ClientsController do
 	describe "GET show" do
 		let(:client)  {FactoryGirl.create(:client)}
 
-		it "renders show template" do
+		it "renders :show template" do
 			#get :show, id: client.id
 			get :show, id: client
 
@@ -72,11 +114,11 @@ describe ClientsController do
 			let (:invalid_data) { FactoryGirl.attributes_for(:client, name: '') }
 			let (:invalid_data2) { FactoryGirl.attributes_for(:client, street_address: '') }
 
-			it "renders new template" do
+			it "renders :new template" do
 				post :create, client: invalid_data
 				expect(response).to render_template(:new)
 			end
-			it "renders new template#{2}" do
+			it "renders :new template#{2}" do
 				post :create, client: invalid_data2
 				expect(response).to render_template(:new)
 			end
@@ -90,6 +132,20 @@ describe ClientsController do
 					post :create, client: invalid_data2
 				}.not_to change(Client, :count)
 			end
+		end
+	end
+
+	describe "DELETE destroy" do
+		let(:client)  {FactoryGirl.create(:client)}
+
+		it "redirects to clients#index" do
+			delete :destroy, id: client
+			expect(response).to redirect_to(clients_path)
+		end
+
+		it "deletes clients from db" do
+			delete :destroy, id: client
+			expect(Client.exists?(client.id)).to be_falsy
 		end
 	end
 end
