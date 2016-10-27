a3bec20d26389d0334ef9b5ded6bdffd58cb61e0
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-10-27 00:53:50 -0400

    Adding service model specs

---
+++ spec/models/services_model_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Service, type: :model do
+
+	describe 'validations' do
+		client = FactoryGirl.create(:client)
+
+		it 'requires client_id' do
+			service = Service.new(client_id: '')
+			expect(service.valid?).to be_falsy
+		end
+		it 'requires price' do
+			service = Service.new(price: '')
+			service.valid?
+			expect(service.errors[:price]).not_to be_empty
+		end
+		describe 'associations' do
+			it 'has belongs_to client association' do
+				service = FactoryGirl.create(:service, client_id: client.id)
+				expect(service.client).to eq(client)
+			end
+		end
+	end
+end
