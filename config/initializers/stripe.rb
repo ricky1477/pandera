8c08a02c2cb9bf0ce88132ced894eef7fe03671f
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-11-05 18:55:11 -0400

    Adding live STRIPE keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,13 @@
-Rails.configuration.stripe = {
-      :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-      :secret_key      => ENV['STRIPE_SECRET_KEY']
-}
+if(Rails.env == 'development' || Rails.env == 'staging' || Rails.env == 'test')
+	Rails.configuration.stripe = {
+						:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+						:secret_key      => ENV['STRIPE_TEST_SECRET_KEY']
+	}
+elsif(Rails.env == 'production')
+	Rails.configuration.stripe = {
+						:publishable_key => ENV['STRIPE_LIVE_PUBLISHABLE_KEY'],
+						:secret_key      => ENV['STRIPE_LIVE_SECRET_KEY']
+	}
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
