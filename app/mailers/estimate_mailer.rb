25e804defd29015bf8995bb8a6629ec2a11525f6
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-11-17 16:15:38 -0500

    Adding estimates mailer

---
+++ app/mailers/estimate_mailer.rb
@@ -0,0 +1,7 @@
+class EstimateMailer < ApplicationMailer
+    def estimate_created(estimate)
+       @estimate = estimate
+       mail to: estimate.client.phone+estimate.client.sms_gateway,
+            subject: 'Your estimate is ready to be seen!'
+    end
+end
