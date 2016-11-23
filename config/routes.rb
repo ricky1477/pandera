c7b617d085afc87bd97350d0e52205571ca4954d
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2016-11-23 17:32:58 -0500

    Still playing with iPhone favicon

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
     get 'googlea87d8b3af5e98ec8.html', :to => redirect('googlea87d8b3af5e98ec8.html')
     get 'favicon.ico', :to => redirect('https://www.dropbox.com/s/92xvznr5p84zswn/favicon-32x32.png?dl=0&raw=1')
     #get 'apple-touch-icon.png', :to => redirect('https://www.dropbox.com/s/92xvznr5p84zswn/favicon-32x32.png?dl=0&raw=1')
-    get 'apple-touch-icon.png', :to => redirect('https://www.dropbox.com/s/8hzrto5aroel0z7/mstile-150x150.png?dl=0&raw=1')
+    get 'apple-touch-icon-120x120.png', :to => redirect('https://www.dropbox.com/s/8hzrto5aroel0z7/mstile-150x150.png?dl=0&raw=1')
 end
