e8b1c69fbed57c308ec2283e290066d64fe3dd7d
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-05-08 00:32:12 -0400

    Merge from master: Fixing client-schedules to show address

--- app/models/client_schedule.rb
+++ app/models/client_schedule.rb
@@ -1,4 +1,9 @@
 class ClientSchedule < ActiveRecord::Base
     belongs_to :schedule
     belongs_to :client
+    before_create :set_client_id
+
+    def set_client_id
+      self.client_id = Client.find_by_street_address(self.client_street_address).id
+    end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,5 @@
 class Schedule < ActiveRecord::Base
+    validates :date, presence: true
     has_many :client_schedules, dependent: :destroy
-    accepts_nested_attributes_for :client_schedules, :reject_if => lambda { |a| a[:client_id].blank? }, :allow_destroy => true
+    accepts_nested_attributes_for :client_schedules, :reject_if => lambda { |a| a[:client_street_address].blank? }, :allow_destroy => true
 end
