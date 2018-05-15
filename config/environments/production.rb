f4169470b8f5f262b19f67d77025701c02c01d8c
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-05-14 22:55:40 -0400

    Resetting SMTP to Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,24 +82,15 @@ Rails.application.configure do
   # Mailer
   #config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
-  #  address:              'smtp.gmail.com',
-  #  port:                 587,
-  #  domain:               'gmail.com',
-  #  user_name:            ENV["GMAIL_USER_NAME"],
-  #  password:             ENV["GMAIL_PASSWORD"],
-  #  authentication:       'plain',
-  #  openssl_verify_mode:  'none',
-  #  enable_starttls_auto: true  }
-	ActionMailer::Base.smtp_settings = {
-	  :port           => ENV['MAILGUN_SMTP_PORT'],
-	  :address        => ENV['MAILGUN_SMTP_SERVER'],
-	  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-	  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-	  :domain         => 'yourapp.heroku.com',
-	  :authentication => :plain,
-	}
-	ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_USER_NAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    openssl_verify_mode:  'none',
+    enable_starttls_auto: true  }
 
   config.action_mailer.perform_deliveries = true
 
