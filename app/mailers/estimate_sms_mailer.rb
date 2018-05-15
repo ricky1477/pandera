1cdc19080fe793511d7beb9f1ce3ec5cd654882e
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-05-14 22:52:16 -0400

    Testing daily reporting

--- app/mailers/estimate_sms_mailer.rb
+++ app/mailers/estimate_sms_mailer.rb
@@ -2,7 +2,7 @@ class EstimateSmsMailer < ApplicationMailer
    def estimate_created(estimate)
        @estimate = estimate
        mail to: estimate.client.curated_phone+estimate.client.sms_gateway,
-            cc: estimate.client.curated_phone2+estimate.client.sms_gateway2,
+            #cc: estimate.client.curated_phone2+estimate.client.sms_gateway2,
             subject: 'Your estimate is ready to be seen!'
    end
 end
