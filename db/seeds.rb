df597d37bfc7927a50606326433a5d470d5b88ba
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-07-12 17:03:37 -0400

    Fixing no data issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ShippingAddress.create(name: "The Rick Company", street: "Money St", city: "Silver Dollars", state: "MD", zipcode: "20906", is_default: true, email: "rickreyhsig@hotmail.com", phone: "($$$)$$$-$$$$")
+
