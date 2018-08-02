88fc3c52ed9546c9042237122edb55d79c36c711
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-08-02 12:35:24 -0400

    Renaming client_schedule service to service_type

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,6 +8,10 @@ class Service < ActiveRecord::Base
 
       DESCRIPTION = ['Weekly Cutting', 'Bi-weekly Cutting', 'Mulching', 'Gutter Cleaning', 'Trimming Bushes and Shrubs','Fall Clean-Up',
       'Fertilizer', 'Aeration and Seeding', 'Power Washing', 'Snow Removal', 'Other']
+      TIME = ['5:00am','5:30am','6:00am','6:30am','7:00am','7:30am','8:00am','8:30am',
+              '9:00am','9:30am','10:00am','10:30am','11:00am','11:30am',
+              '12:00am','12:30am','1:00pm','1:30pm','2:00pm','2:30pm',
+              '12:00pm','12:30pm','1:00pm','1:30pm','2:00pm','2:30pm']
 
     def self.search(search)
         if search
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726223410) do
+ActiveRecord::Schema.define(version: 20180801193027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20180726223410) do
     t.datetime "updated_at",            null: false
     t.string   "client_street_address"
     t.datetime "time"
-    t.string   "type"
+    t.string   "service_type"
     t.string   "notes"
   end
 
