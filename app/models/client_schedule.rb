d6e5a03161329388641601cf8eb94b876e70411b
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-08-03 23:18:26 -0400

    Working on scheduling

--- app/models/client_schedule.rb
+++ app/models/client_schedule.rb
@@ -5,5 +5,8 @@ class ClientSchedule < ActiveRecord::Base
 
     def set_client_id
       self.client_id = Client.find_by_street_address(self.client_street_address).id
+      #self.time = self.time
+      #self.service_type = self.service_type
+      #self.notes = self.notes
     end
 end
