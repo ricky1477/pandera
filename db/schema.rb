847bb50ec9f688707e33ed25b9b06e99b6f83aa0
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-08-09 12:28:48 -0400

    Üpdating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801193027) do
+ActiveRecord::Schema.define(version: 20180807165722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20180801193027) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.string   "client_street_address"
-    t.datetime "time"
+    t.string   "time"
     t.string   "service_type"
     t.string   "notes"
   end
