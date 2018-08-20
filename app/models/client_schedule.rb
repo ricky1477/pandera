e81250b2a06ee3854ddb85cbca774cbe46284688
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-08-20 19:05:38 -0400

    Create service when client_schedule is updated and marked done

--- app/models/client_schedule.rb
+++ app/models/client_schedule.rb
@@ -1,17 +1,23 @@
 class ClientSchedule < ActiveRecord::Base
     belongs_to :schedule
     belongs_to :client
-    has_one :service
+    belongs_to :service
     before_create :set_client_id
     after_save :create_new_services
 
     def create_new_services
-      Rails.logger.info '---TESTING---'
-      Rails.logger.info self.inspect
+      #Rails.logger.info '---TESTING---'
+      #Rails.logger.info self.inspect
       if self.done && self.done_changed?
-        Rails.logger.info '---CHANGED---'
-        # Add service_id as field with has_one relationship
-        #Service.create(id: integer, date: date, description: text, price: decimal, maturity: date, created_at: datetime, updated_at: datetime, client_id: integer, paid: boolean, invoice_id: integer, notes: text)
+        #Rails.logger.info '---CHANGED---'
+        #Rails.logger.info self.inspect
+        s = Service.create(date: self.schedule.date, client_id: self.client_id, notes: self.notes, description: self.service_type)
+        #Rails.logger.info '---SERVICE---'
+        #Rails.logger.info s.inspect
+        #Rails.logger.info self.inspect
+
+        ## TODO Add service price
+        self.update_columns(service_id: s.id) # Doesn't call callback
       end
     end
 
