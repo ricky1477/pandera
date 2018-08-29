552524f809207adb49156450ac6d0b4cf27ed68c
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-08-29 15:31:28 -0400

    Cleaning up client_schedule callback and form

--- app/models/client_schedule.rb
+++ app/models/client_schedule.rb
@@ -6,16 +6,8 @@ class ClientSchedule < ActiveRecord::Base
     after_save :create_new_services
 
     def create_new_services
-      #Rails.logger.info '---TESTING---'
-      #Rails.logger.info self.inspect
       if self.done && self.done_changed?
-        #Rails.logger.info '---CHANGED---'
-        #Rails.logger.info self.inspect
         s = Service.create(date: self.schedule.date, client_id: self.client_id, notes: self.notes, description: self.service_type)
-        #Rails.logger.info '---SERVICE---'
-        #Rails.logger.info s.inspect
-        #Rails.logger.info self.inspect
-
         ## TODO Add service price
         self.update_columns(service_id: s.id) # Doesn't call callback
       end
