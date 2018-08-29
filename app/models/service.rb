425f5c272593e337ef852a4d524d4d35139828b0
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-08-29 16:22:55 -0400

    Merge from master: Adding fields to client_schedules, connecting when done to service and adding service_types to DB

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,13 +1,18 @@
 class Service < ActiveRecord::Base
       belongs_to :client
       belongs_to :invoice
-      validates :price, presence: true
+      #validates :price, presence: true
       validates :client_id, presence: true
       before_destroy :check_invoice_association
       before_save :price
 
       DESCRIPTION = ['Weekly Cutting', 'Bi-weekly Cutting', 'Mulching', 'Gutter Cleaning', 'Trimming Bushes and Shrubs','Fall Clean-Up',
       'Fertilizer', 'Aeration and Seeding', 'Power Washing', 'Snow Removal', 'Other']
+      TIME = ['5:00am','5:30am','6:00am','6:30am','7:00am','7:30am','8:00am','8:30am',
+              '9:00am','9:30am','10:00am','10:30am','11:00am','11:30am',
+              '12:00am','12:30am','1:00pm','1:30pm','2:00pm','2:30pm',
+              '3:00pm','3:30pm','4:00pm','4:30pm','5:00pm','5:30pm',
+              '6:00pm','6:30pm','7:00pm','7:30pm','8:00pm','8:30pm']
 
     def self.search(search)
         if search
