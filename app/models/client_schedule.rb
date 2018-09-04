4eedb534763c764dc731363a6f52886282b062b0
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-09-04 18:05:41 -0400

    Create service when marked as done and delete dependent

--- app/models/client_schedule.rb
+++ app/models/client_schedule.rb
@@ -1,15 +1,14 @@
 class ClientSchedule < ActiveRecord::Base
     belongs_to :schedule
     belongs_to :client
-    belongs_to :service
+    belongs_to :service, dependent: :destroy
     before_create :set_client_id
     after_save :create_new_services
     validates_presence_of :price, :if => :done?, :message => "should be present to be marked as done."
 
     def create_new_services
-      if self.done && self.done_changed?
-        s = Service.create(date: self.schedule.date, client_id: self.client_id, notes: self.notes, description: self.service_type)
-        # TODO Add service price
+      if self.done && self.done_changed? && self.service_id.nil?
+        s = Service.create(date: self.schedule.date, client_id: self.client_id, notes: self.notes, description: self.service_type, price: self.price.to_s)
         self.update_columns(service_id: s.id) # Doesn't call callback
       end
     end
