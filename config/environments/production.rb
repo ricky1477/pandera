20eb01a8fa79b25bcc8d2833ba5727391aca49e1
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-09-25 11:58:54 -0400

    Set prod root_url with HTTPS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'pandera.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://pandera.herokuapp.com' }
 
   # Mailer
   config.action_mailer.raise_delivery_errors = false
